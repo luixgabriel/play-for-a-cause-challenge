3fbb0575d5e1ad924d5b5decb69296dfce496b66
Luis Gabriel <luis16757@gmail.com>
2023-11-29 08:27:48 -0300

    chore: organizando rotas e serviços de usuário

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsOptional()
+  id: string;
+
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  imageUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
